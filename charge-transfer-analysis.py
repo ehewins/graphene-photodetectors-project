cc78bb8bf220ba2ee46de38a7a8271c7650d3732
Ellis Hewins <ellishewins@gmail.com>
2024-05-05 17:37:35 +0100

    Got the fits for the experiment on 15/03 working, and made some general tweaks

--- charge-transfer-analysis.py
+++ charge-transfer-analysis.py
@@ -62,13 +62,13 @@ param_list, error_list, fit_points_list = triple_fitting_process(region_t,
                                                                  region_I,
                                                                  init_params)
 fig, ax = plt.subplots(1, 1, figsize=(12, 7))
-ax.set_title("Fitting with $I_{sd} = I_0 + \\sum_{i=1}^N a_i [\\exp(-t/\\tau_i) - 1]$")
+# ax.set_title("Fitting with $I_{sd} = I_0 + \\sum_{i=1}^N a_i [\\exp(-t/\\tau_i) - 1]$")
 ax.set_xlabel("Time, $t$ (s)")
-ax.set_ylabel("Source-drain current, $I_{sd}$ (A)")
-ax.plot(region_t, region_I, color='k', linestyle='-', label='Data')
+ax.set_ylabel("Source-drain current, $I_{sd}$ ($\\mu$A)")
+ax.plot(region_t, 1e6*region_I, color='k', linestyle='-', label='Data')
 for n in range(3):
-    ax.plot(region_t, fit_points_list[n], color=f'C{n}', linestyle='dotted',
-            linewidth=3, label=f'Fit w/ $N={n+1}$')
+    ax.plot(region_t, 1e6*fit_points_list[n], color=f'C{n}', linestyle='dotted',
+            linewidth=3, label=f'Fit w/ {n+1} exponent{"s" if n > 0 else ""}')
     print("Fit params. for "+["one", "two", "three"][n]+" exponentials:")
     params, errors = param_list[n], error_list[n]
     for i in range(len(params)//2):
@@ -78,6 +78,7 @@ for n in range(3):
         result_string += f"tau_{i+1:d} = {tau:.2f} ± {tau_err:.2f} s."
         print(result_string)
 ax.legend()
+fig.tight_layout()
 
 print()
 print("For the second region, equilibrating from light on to light off state:")
@@ -88,13 +89,13 @@ param_list, error_list, fit_points_list = triple_fitting_process(region_t,
                                                                  region_I,
                                                                  init_params)
 fig, ax = plt.subplots(1, 1, figsize=(12, 7))
-ax.set_title("Fitting with $I_{sd} = I_0 + \\sum_{i=1}^N a_i [\\exp(-t/\\tau_i) - 1]$")
+# ax.set_title("Fitting with $I_{sd} = I_0 + \\sum_{i=1}^N a_i [\\exp(-t/\\tau_i) - 1]$")
 ax.set_xlabel("Time, $t$ (s)")
-ax.set_ylabel("Source-drain current, $I_{sd}$ (A)")
-ax.plot(region_t, region_I, color='k', linestyle='-', label='Data')
+ax.set_ylabel("Source-drain current, $I_{sd}$ ($\\mu$A)")
+ax.plot(region_t, 1e6*region_I, color='k', linestyle='-', label='Data')
 for n in range(3):
-    ax.plot(region_t, fit_points_list[n], color=f'C{n}', linestyle='dotted',
-            linewidth=3, label=f'Fit w/ $N={n+1}$')
+    ax.plot(region_t, 1e6*fit_points_list[n], color=f'C{n}', linestyle='dotted',
+            linewidth=3, label=f'Fit w/ {n+1} exponent{"s" if n > 0 else ""}')
     print("Fit params. for "+["one", "two", "three"][n]+" exponentials:")
     params, errors = param_list[n], error_list[n]
     for i in range(len(params)//2):
@@ -104,6 +105,7 @@ for n in range(3):
         result_string += f"tau_{i+1:d} = {tau:.2f} ± {tau_err:.2f} s."
         print(result_string)
 ax.legend()
+fig.tight_layout()
 
 # """
 # Objective 2:
@@ -170,98 +172,89 @@ ax.legend()
 #         print(result_string)
 # ax.legend()
 
-# """
-# Objective 3:
-# Repeat this fitting process for the fast charge transfer processes observed in
-# Device 1 on 15/03/24, before the UV-induced changes made them unmeasurable.
-# """
+"""
+Objective 3:
+Repeat this fitting process for the fast charge transfer processes observed in
+Device 1 on 15/03/24, before the UV-induced changes made them unmeasurable.
+"""
 
-# print()
-# print("========== First Post-Functionalisation Time Experiments [15/03/24] ==========")
-# print()
-# time, current = np.loadtxt("data/cvd_func_15032024/CVD1F-time.dat",
-#                            usecols=(1, 3), unpack=True)
-# # This array defines the indices bouding each on/off region
-# region_indices = [29, 56, 84, 110, 138, 163, 187, 214, 240]
-# # We want to remove some other, even faster effects, and the slow effects.
-# for problem_index in region_indices[0:8:2]:
-#     current[problem_index] = current[problem_index - 1]
-# (m, c), cv = curve_fit(linear_fit, time, current)
-# current -= m * time  # subtract (approximately) linear slow effect.
+print()
+print("========== First Post-Functionalisation Time Experiments [15/03/24] ==========")
+print()
+time, current = np.loadtxt("data/cvd_func_15032024/CVD1F-time.dat",
+                           usecols=(1, 3), unpack=True)
+# This array defines the indices bounding each on/off region
+region_indices = [29, 56, 84, 110, 138, 163, 187, 214, 240]
+# We want to remove some other, even faster effects, and the slow effects.
+for problem_index in region_indices[0:8:2]:
+    current[problem_index] = current[problem_index - 1]
+(m, c), cv = curve_fit(linear_fit, time, current)
+current -= m * time  # subtract (approximately) linear slow effect.
 
-# fig, ax = plt.subplots(1, 1, figsize=(12, 7))
+fig, ax = plt.subplots(1, 1, figsize=(12, 7))
 # ax.set_title("Fitting with $I_{sd} = I_0 + \\sum_{i=1}^N a_i [\\exp(-t/\\tau_i) - 1]$")
-# ax.set_xlabel("Time, $t$ (s)")
-# ax.set_ylabel("Source-drain current, $I_{sd}$ (A)")
-# ax.plot(time, current, color='k', linestyle='-', label='Data')
-# singles, doubles, triples = [], [], []
-# labelled = False
-# # init_params = np.tile((2e-8, 2e-1), 3)
-# for i in range(len(region_indices)-1):
-#     region_start, region_end = region_indices[i], region_indices[i+1]
-#     region_t = np.array(time[region_start: region_end])
-#     region_I = np.array(current[region_start: region_end])
-#     # init_params[::2] *= -1
-#     param_list, error_list, fit_points_list = triple_fitting_process(region_t,
-#                                                                      region_I)
-#     singles.append(param_list[0])
-#     doubles.append(param_list[1])
-#     triples.append(param_list[2])
-#     for n in range(3):
-#         ax.plot(region_t, fit_points_list[n], color=f'C{n}',
-#                 linestyle='dotted', linewidth=3,
-#                 label=f'Fit w/ $N={n+1}$' if not labelled else None)
-#     labelled = True
-# ax.legend()
-# on_mean_1, on_std_1 = np.mean(singles[::2], axis=0), np.std(singles[::2], axis=0)
-# off_mean_1, off_std_1 = np.mean(singles[1::2], axis=0), np.std(singles[1::2], axis=0)
-# on_mean_2, on_std_2 = np.mean(doubles[::2], axis=0), np.std(doubles[::2], axis=0)
-# off_mean_2, off_std_2 = np.mean(doubles[1::2], axis=0), np.std(doubles[1::2], axis=0)
-# on_mean_3, on_std_3 = np.mean(triples[::2], axis=0), np.std(triples[::2], axis=0)
-# off_mean_3, off_std_3 = np.mean(triples[1::2], axis=0), np.std(triples[1::2], axis=0)
-# print("For the light-on regions:")
-# print("Fit params. for one exponential:")
-# for i in range(1):
-#     a, a_err = on_mean_1[2 * i], on_std_1[2 * i]
-#     tau, tau_err = on_mean_1[2 * i + 1], on_std_1[2 * i + 1]
-#     result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
-#     result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
-#     print(result_string)
-# print("Fit params. for two exponentials:")
-# for i in range(2):
-#     a, a_err = on_mean_2[2 * i], on_std_2[2 * i]
-#     tau, tau_err = on_mean_2[2 * i + 1], on_std_2[2 * i + 1]
-#     result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
-#     result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
-#     print(result_string)
-# print("Fit params. for three exponentials:")
-# for i in range(3):
-#     a, a_err = on_mean_3[2 * i], on_std_3[2 * i]
-#     tau, tau_err = on_mean_3[2 * i + 1], on_std_3[2 * i + 1]
-#     result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
-#     result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
-#     print(result_string)
-# print("For the light-off regions:")
-# print("Fit params. for one exponential:")
-# for i in range(1):
-#     a, a_err = off_mean_1[2 * i], off_std_1[2 * i]
-#     tau, tau_err = off_mean_1[2 * i + 1], off_std_1[2 * i + 1]
-#     result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
-#     result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
-#     print(result_string)
-# print("Fit params. for two exponentials:")
-# for i in range(2):
-#     a, a_err = off_mean_2[2 * i], off_std_2[2 * i]
-#     tau, tau_err = off_mean_2[2 * i + 1], off_std_2[2 * i + 1]
-#     result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
-#     result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
-#     print(result_string)
-# print("Fit params. for three exponentials:")
-# for i in range(3):
-#     a, a_err = off_mean_3[2 * i], off_std_3[2 * i]
-#     tau, tau_err = off_mean_3[2 * i + 1], off_std_3[2 * i + 1]
-#     result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
-#     result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
-#     print(result_string)
+ax.set_xlabel("Time, $t$ (s)")
+ax.set_ylabel("Source-drain current, $I_{sd}$ ($\\mu$A)")
+ax.plot(time, 1e6*current, color='k', linestyle='-', label='Data')
+singles, doubles = [], []
+unlabelled = 2
+init_params_1 = [-6e-8, 1e-2, -2e-8, 8e-1]
+init_params_2 = [3e-8, 1, 5e-8, 1e-1]
+for n, (region_start, region_end) in enumerate(zip(region_indices[:-1],
+                                                   region_indices[1:])):
+    region_t = np.array(time[region_start: region_end])
+    region_I = np.array(current[region_start: region_end])
+    fit_t = region_t - region_t[0]
+    fit_I = region_I - region_I[0]
+    init_params = init_params_1 if n % 2 == 0 else init_params_2
+    for fit_func, storage_array, p0, N in zip((single_exp, double_exp),
+                                              (singles, doubles),
+                                              (None, init_params),
+                                              (1, 2)):
+        params, cv = curve_fit(fit_func, fit_t, fit_I, p0=p0)
+        smooth_t = np.linspace(fit_t[0], fit_t[-1], 50)
+        smooth_I = fit_func(smooth_t, *params)
+        label = f"Fits w/ {N} exponent{'s' if N > 1 else ''}"
+        ax.plot(smooth_t + region_t[0], 1e6*(smooth_I + region_I[0]), ':',
+                label=label if unlabelled else None,
+                color=f'C{(N-1)*3}', linewidth=2.5)
+        unlabelled -= 1 if unlabelled != 0 else 0
+        storage_array.append(params)
+ax.legend(loc="upper right")
+fig.tight_layout()
+on_mean_1, on_std_1 = np.mean(singles[::2], axis=0), np.std(singles[::2], axis=0)
+off_mean_1, off_std_1 = np.mean(singles[1::2], axis=0), np.std(singles[1::2], axis=0)
+on_mean_2, on_std_2 = np.mean(doubles[::2], axis=0), np.std(doubles[::2], axis=0)
+off_mean_2, off_std_2 = np.mean(doubles[1:-1:2], axis=0), np.std(doubles[1:-1:2], axis=0)
+print("For the light-on regions:")
+print("Fit params. for one exponential:")
+for i in range(1):
+    a, a_err = on_mean_1[2 * i], on_std_1[2 * i]
+    tau, tau_err = on_mean_1[2 * i + 1], on_std_1[2 * i + 1]
+    result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
+    result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
+    print(result_string)
+print("Fit params. for two exponentials:")
+for i in range(2):
+    a, a_err = on_mean_2[2 * i], on_std_2[2 * i]
+    tau, tau_err = on_mean_2[2 * i + 1], on_std_2[2 * i + 1]
+    result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
+    result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
+    print(result_string)
+print("For the light-off regions:")
+print("Fit params. for one exponential:")
+for i in range(1):
+    a, a_err = off_mean_1[2 * i], off_std_1[2 * i]
+    tau, tau_err = off_mean_1[2 * i + 1], off_std_1[2 * i + 1]
+    result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
+    result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
+    print(result_string)
+print("Fit params. for two exponentials:")
+for i in range(2):
+    a, a_err = off_mean_2[2 * i], off_std_2[2 * i]
+    tau, tau_err = off_mean_2[2 * i + 1], off_std_2[2 * i + 1]
+    result_string = f"a_{i+1} = {a:.2e} ± {a_err:.2e} A, "
+    result_string += f"tau_{i+1} = {tau:.2f} ± {tau_err:.2f} s."
+    print(result_string)
 
 plt.show()
